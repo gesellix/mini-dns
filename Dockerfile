b628a4da1c3737493fc9adf2633375530bc438ea
Tobias Gesellchen <tobias@gesellix.de>
2015-11-14 01:12:22 +0100

    refactor everything

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,11 @@ ENV GOPATH /go
 ENV APPPATH $GOPATH/src/github.com/gesellix/mini-dns
 COPY . $APPPATH
 
-RUN apk add --update -t build-deps go git mercurial libc-dev gcc libgcc \
-    && cd $APPPATH && go get -d && go build -o /bin/mini-dns \
-    && apk del --purge build-deps && rm -rf $GOPATH
+RUN echo '@community http://dl-4.alpinelinux.org/alpine/edge/community' >> /etc/apk/repositories
+RUN apk upgrade --update --available && apk add go@community git mercurial libc-dev gcc libgcc
+RUN cd $APPPATH && go get -d && go build -o /bin/mini-dns
+#RUN cd $APPPATH && go get -d && go build -o /bin/mini-dns \
+#    && apk del --purge build-deps && rm -rf $GOPATH
 
 ENTRYPOINT [ "/bin/mini-dns" ]
 CMD [ "-port=5353", "-printf=true" , "-debug=true" ]
--- mini-dns.go
+++ mini-dns.go
@@ -2,86 +2,89 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"log"
 	"net"
 	"os"
 	"os/signal"
 	"strconv"
 	"syscall"
 
+	"github.com/golang/glog"
 	"github.com/miekg/dns"
+	"log"
 )
 
-var (
-	printf   *bool
-	debug   *bool
-)
+func newRR(host string, addr net.IP) (dns.RR) {
+	rr := new(dns.A)
+	rr.Hdr = dns.RR_Header{
+		Name: host,
+		Rrtype: dns.TypeA,
+		Class: dns.ClassINET,
+		Ttl: 0}
+	rr.A = addr.To4()
+	return rr
+}
 
-func handleHosts(w dns.ResponseWriter, r *dns.Msg) {
-	firstQuestion := r.Question[0].Name
-	if *debug {
-		fmt.Printf("// question[0]: %v\n", firstQuestion)
+func findFirstIPv4(addrs []string) (match string) {
+	match = ""
+	if len(addrs) == 0 {
+		return
 	}
+	for _, match = range addrs {
+		if net.ParseIP(match).To4() != nil {
+			return
+		}
+	}
+	return
+}
 
-	var (
-		rr dns.RR
-		a net.IP
-	)
+func handleFirstHost(w dns.ResponseWriter, r *dns.Msg) {
+	firstQuestion := r.Question[0].Name
+	glog.Infof("first question: %v\n", firstQuestion)
 
 	m := new(dns.Msg)
 	m.SetReply(r)
 
 	addrs, _ := net.LookupHost(firstQuestion)
-	if *debug {
-		fmt.Printf("// hosts: %v\n", addrs)
-	}
-	if (len(addrs) > 0) {
-		match := addrs[0]
-		if (len(addrs) > 1 && net.ParseIP(match).To4() == nil) {
-			// TODO choose IPv4 by default (iterate over all addrs, select the first IPv4 address)
-			match = addrs[1]
-		}
-		if *printf {
-			fmt.Printf("// match: %v -> %v\n", firstQuestion, match)
-		}
-		a = net.ParseIP(match)
-		if *debug {
-			fmt.Printf("// addr: %v\n", a.To4())
-		}
+	glog.Infof("hosts: %v\n", addrs)
+	log.Printf("hosts(%v): %v\n", firstQuestion, addrs)
+	match := findFirstIPv4(addrs)
+	if (match != "") {
+		glog.Infof("match: %v -> %v\n", firstQuestion, match)
+		a := net.ParseIP(match)
+		glog.Infof("addr: %v\n", a.To4())
 
-		rr = new(dns.A)
-		rr.(*dns.A).Hdr = dns.RR_Header{
-			Name: firstQuestion,
-			Rrtype: dns.TypeA,
-			Class: dns.ClassINET,
-			Ttl: 0}
-		rr.(*dns.A).A = a.To4()
-
+		rr := newRR(firstQuestion, a)
 		m.Answer = append(m.Answer, rr)
 		m.Authoritative = true
+		//	} else {
+		//		import 	"github.com/bogdanovich/dns_resolver"
+		//		glog.Infof("fallback via resolv.conf...")
+		//		resolver, _ := dns_resolver.NewFromResolvConf("/etc/resolv.conf")
+		//		result, _ := resolver.LookupHost(firstQuestion)
+		//		glog.Infof("...result: %v\n", result)
+		//		if len(result) > 0 {
+		//			rr = newRR(firstQuestion, result[0])
+		//			m.Answer = append(m.Answer, rr)
+		//			m.Authoritative = true
+		//		}
 	}
 
-	if *debug {
-		fmt.Printf("// response\n%v\n", m.String())
-	}
+	glog.Infof("response\n%v\n", m.String())
 
 	w.WriteMsg(m)
 }
 
 func main() {
 	port := flag.Int("port", 5353, "port to run on")
-	printf = flag.Bool("print", true, "print matched addresses")
-	debug = flag.Bool("debug", false, "print debug logs")
 	flag.Parse()
 
-	dns.HandleFunc(".", handleHosts)
+	dns.HandleFunc(".", handleFirstHost)
 
 	serve := func(net string, port int) {
 		server := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: net}
 		err := server.ListenAndServe()
 		if err != nil {
-			fmt.Printf("Failed to setup the " + net + " server: %s\n", err.Error())
+			glog.Infof("Failed to setup the " + net + " server: %s\n", err.Error())
 		}
 	}
 
